0580d2279ba6c2919dff83ee085a80d6c08b9a51
Sarcinator <88929204+Sarcinator@users.noreply.github.com>
2021-08-14 12:28:53 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+# x = Random vector with x numbers
+# y = Numerical value for number of rows
 
-## Write a short comment describing this function
+# This function creates a matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x, nrow=y){ 
+  new_matrix <- matrix(x,nrow =y)
+  print(new_matrix)
 }
 
 
-## Write a short comment describing this function
+# Output from makeCachematrix can be stored in new_matrix and be used in solveCache
+# This function transforms the matrix in its inverse and tries to create an Identity matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(new_matrix){
+  solve_matrix<- solve(new_matrix)
+  cache <-solve_matrix %*% new_matrix
+  Comparison <- diag(y)               # Comparison = Matrix to compare with cache
+  if (identical(cache,Comparison)==FALSE){  #Diag(y) creates an Identity matrix with a number of y rows and y columns
+    stop(cacheSolve)
+  }
+  else {
+    return(cache)  # Tests, if cache is an Identity matrix. If not, then an error is returned
+  }
 }
